85475fd089ae8384c10d143874a2511a73bc52aa
jhjung_dev <jhjung@monki.net>
2025-05-23 16:31:35 +0900

    Initial Commit

---
+++ DorayCompanion/DorayCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DorayCompanionApp.swift
+//  DorayCompanion
+//
+//  Created by monthlykitchen on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct DorayCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DorayCompanionTests/DorayCompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  DorayCompanionTests.swift
+//  DorayCompanionTests
+//
+//  Created by monthlykitchen on 5/23/25.
+//
+
+import Testing
+@testable import DorayCompanion
+
+struct DorayCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
